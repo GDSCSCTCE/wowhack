66a42c80547912f3a6cd566b2794de74a78cea3b
sreedhar <sreedharks2003@gmail.com>
2024-03-17 19:42:50 +0530

    feat : Jsdoc in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,14 @@
 import { Button } from "./ui/button";
+/**
+ * @todo Login button is currenlty commented !
+ * @returns ReactNode
+ */
 export default function NavBar() {
   return (
     <div className="flex flex-col dark">
       <div className="flex flex-row justify-evently gap-4 h-10 m-5 p-2">
         <h1 className="text-[18px] leading-8">GDSC WoW</h1>
-        <Button variant={'outline'}>About</Button>
+        {/* <Button variant={'outline'}>Login</Button> */}
       </div>
       <hr className="bg-white"/>
     </div>
