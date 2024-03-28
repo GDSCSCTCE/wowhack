3ed40a590a940967916eae1d26854a0e3065e542
Rohit George <code.mastermind.07@gmail.com>
2024-03-28 17:26:42 +0530

    hero section of landing page

--- components/LandingPage/Hero.tsx
+++ components/LandingPage/Hero.tsx
@@ -3,7 +3,9 @@ import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
-    <div className="flex h-screen w-full flex-col justify-center items-center md:p-5 px-10 pt-28">
+    <div className="flex h-screen w-full flex-col justify-center items-center md:p-5 px-10 pt-28 overflow-hidden relative">
+        <img src="/assets/images/LineDraw2.svg" className="gradImgR absolute opacity-45 bg-gradient-to-r from-bgColor to-transparent -right-40 z-10 -rotate-45 -top-24  w-[50vw]" alt="" />
+        <img src="/assets/images/LineDraw2.svg" className=" gradImgL absolute  opacity-45 -left-40 z-10 -rotate-45 scale-x-[-1]  scale-y-[-1]  -bottom-24  w-[50vw]" alt="" />
       <h1 className="text-white font-medium text-2xl text-center lg:text-[30px] flex flex-wrap justify-center items-center">
         <span className="text-green-600 px-1">Google</span>
         <span className="text-blue-600 px-1">Developers</span>
@@ -13,7 +15,7 @@ const Hero = () => {
       <p className="text-white font-normal md:text-[25px] text-md my-4">
         presents
       </p>
-      <h1 className="bg-gradient-to-r from-green-600 via-blue-500 to-yellow-400 inline-block text-transparent bg-clip-text font-bold  md:text-[100px] text-5xl lg:text-[130px] mb-4">
+      <h1 className="bg-gradient-to-r from-green-600 via-blue-500 to-yellow-400 inline-block text-transparent bg-clip-text font-bold  md:text-[100px] z-20 text-5xl lg:text-[130px] mb-4">
         WoW'24
       </h1>
       <p className="md:px-56 md:text-xl text-sm lg:text-center text-justify flex justify-center items-center px-4 mb-8">
@@ -23,10 +25,10 @@ const Hero = () => {
         and build solutions for their local communities.
       </p>
       <div className="w-[50vw] flex flex-col md:flex-row justify-end items-center md:justify-center pt-8">
-        <button className="h-20 w-48 gradColor font-medium text-md md:text-xl rounded-2xl md:my-8 my-4 mx-4">
+        <button className="h-20 z-20 w-48 gradColor font-medium text-md md:text-xl rounded-2xl md:my-8 my-4 mx-4">
           Register Now
         </button>
-        <button className="h-20 w-48 gradColor font-medium text-md md:text-xl rounded-2xl md:my-8 my-4 mx-4">
+        <button className="h-20 z-20 w-48 gradColor font-medium text-md md:text-xl rounded-2xl md:my-8 my-4 mx-4">
           Know  more
         
         </button>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,8 @@ import Image from "next/image";
  */
 export default function NavBar() {
   return (
-    <div className="flex flex-col dark fixed  w-full">
-      <div className="flex flex-row justify-evently gap-4 h-10 m-5 p-2">
+    <div className="flex flex-col dark fixed z-50 w-full">
+      <div className="flex flex-row justify-evently gap-4 z-50 h-10 m-5 p-2">
         <img alt="WoW Logo" src={Wow_Circle.src} className="h-10 w-10" />
         <h1 className="text-[25px] leading-10">GDSC WoW</h1>
         {/* <Button variant={'outline'}>Login</Button> */}
