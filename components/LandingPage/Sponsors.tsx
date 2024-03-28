8289ed9494399fba60d3dca43b6078cb6f82eee3
Gautam <gmbodhi@icloud.com>
2024-03-28 20:49:47 +0530

    minor: added links to the sponsors

--- components/LandingPage/Sponsors.tsx
+++ components/LandingPage/Sponsors.tsx
@@ -13,15 +13,21 @@ const Sponsors = () => {
                 <div className="">
                     {/* Make a grid like panel 2 in a row and place a few logos */}
                     <div className="grid grid-cols-2 gap-10">
-                        <div className="bg-white p-5 rounded-lg flex justify-center items-center">
-                            <Image src={Devfolio} alt="DEVFOLIO LOGO" />
-                        </div>
-                        <div className="bg-white p-5 rounded-lg flex justify-center items-center">
-                            <Image src={Polygon} alt="POLYGON LOGO" />
-                        </div>
-                        <div className="bg-white p-5 rounded-lg flex justify-center items-center">
-                            <Image src={ETHIndia} alt="ETHINDIA LOGO" />
-                        </div>
+                        <a href="https://devfolio.co/" target="_blank" rel="noreferrer">
+                            <div className="bg-white p-5 rounded-lg flex justify-center items-center">
+                                <Image src={Devfolio} alt="DEVFOLIO LOGO" />
+                            </div>
+                        </a>
+                        <a href="https://polygon.technology/" target="_blank" rel="noreferrer">
+                            <div className="bg-white p-5 rounded-lg flex justify-center items-center">
+                                <Image src={Polygon} alt="POLYGON LOGO" />
+                            </div>
+                        </a>
+                        <a href="https://ethindia.co/" target="_blank" rel="noreferrer">
+                            <div className="bg-white p-5 rounded-lg flex justify-center items-center">
+                                <Image src={ETHIndia} alt="ETHINDIA LOGO" />
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
